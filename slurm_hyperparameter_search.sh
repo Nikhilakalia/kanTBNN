78253d8b36871624d070dffe7607e05d54684c8c
rmcconke <rmcconke@uwaterloo.ca>
2023-05-23 15:35:23 -0400

    added slurm script

---
+++ slurm_hyperparameter_search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --tasks-per-node=1 
+#SBATCH --cpus-per-task=16 # change this parameter to 2,4,6,... to see the effect on performance
+#SBATCH --mem=8G      
+#SBATCH --time=0:5:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=<your account>
+
+module load python/3.10  # Using Default Python version - Make sure to choose a version that suits your application
+source /scratch/rmcconke/training_env/bin/activate
+module load scipy-stack
+
+echo "starting training..."
+
+python3 hyperparam_search.py $1 > hyperparam_search_$1.log
