3fad2b09aaa9b3ebc4766a9cb5af172a343a1d1b
rmcconke <rmcconke@uwaterloo.ca>
2023-06-01 10:32:40 -0400

    reran foam flat plate, back2basics working

--- training_single_run.py
+++ training_single_run.py
@@ -1,32 +1,47 @@
 import pandas as pd
 import torch.nn as nn
 from training_utils import early_stopped_tbnn_training_run
-
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('cluster_number')
+args = parser.parse_args()
+import torch
+import matplotlib.pyplot as plt
+import pickle
+import torch.nn as nn
 dataset_params = {'file': '/home/ryley/WDK/ML/dataset/komegasst_split.csv',
                   'test_set': ['case_1p2','fp_3630'],
-                  'final_val_set': ['case_1p0','fp_2000','fp_3970'],
-                  'cv_val_sets': {0: ['case_0p5','fp_2540','fp_1000'],
-                                  1: ['case_0p8','fp_4060','fp_3270'],
-                                  2: ['case_1p5','fp_1410','fp_3030']}
                 }
 
 df = pd.read_csv(dataset_params['file'])
+args = parser.parse_args()
+cluster = args.cluster_number
+
+if cluster != 'all':
+    cluster = int(args.cluster_number)
+    df = df[df['Cluster']==cluster]
+else:
+    cluster = args.cluster_number
 df_test = df[df['Case'].isin(dataset_params['test_set'])]
-df_final_val = df[df['Case'].isin(dataset_params['final_val_set'])]
-df_tv = df[~df['Case'].isin(dataset_params['test_set']+dataset_params['final_val_set'])]
-print(f'Dataset: {len(df)}, test: {len(df_test)}, final_val: {len(df_final_val)}, tv: {len(df_tv)}')
+df_tv = df[~df['Case'].isin(dataset_params['test_set'])]
+print(f'Dataset: {len(df)}, test: {len(df_test)}, tv: {len(df_tv)}')
 
-training_params = {'early_stopping_patience': 20,
-                'max_epochs': 1000,
-                'learning_rate': 0.001,
-                'learning_rate_decay': 0.98,
-                'batch_size': 128,
-                'val_set':['case_1p5','fp_1410','fp_3030'],
-               }
+training_params = {'early_stopping_patience': 20, 'max_epochs': 1000, 'learning_rate': 0.0001, 'learning_rate_decay': 0.99, 'batch_size': 128, 'val_set': ['case_1p5', 'fp_1410', 'fp_3030']}
 
-model_params = {'neurons': 50,
-                'n_hidden': 5,
-                'activation_function': nn.SiLU(),
-                'input_features': ['komegasst_q6','komegasst_q5','komegasst_I1_1','komegasst_I1_3','komegasst_I2_3','komegasst_I1_5']
-}
-results = early_stopped_tbnn_training_run(model_params,training_params,df_tv)
+training_params['val_set']=['case_1p0','fp_2000','fp_3970']
+model_params = {'neurons': 50, 'n_hidden': 3, 'activation_function': nn.SiLU(), 'input_features': ['komegasst_I1_1', 'komegasst_I1_3', 'komegasst_I2_3', 'komegasst_I1_5']}
+
+
+model, loss_vals, val_loss_vals  = early_stopped_tbnn_training_run(model_params,training_params,df_tv)
+torch.save(model.state_dict(), f'models/cluster_{cluster}')
+
+
+with open(f"models/params_cluster_{cluster}.pickle", 'wb') as handle:
+    pickle.dump(model_params, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+fig, ax = plt.subplots(1,figsize=(5,5))
+ax.plot(loss_vals,'-',color='b')
+ax.plot(val_loss_vals,'--',color='r')
+ax.semilogy()
+fig.tight_layout()
+fig.savefig(f'models/model_cluster_{cluster}.png',dpi=300)
--- training_utils.py
+++ training_utils.py
@@ -16,6 +16,7 @@ device = torch.device("cpu")
 from collections import defaultdict
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import ParameterSampler
+import copy
 
 class EarlyStopper:
     def __init__(self, patience=1, min_delta=0):
@@ -34,11 +35,10 @@ class EarlyStopper:
                 return True
         return False
 
-def bLoss(outputs, labels, alpha = 1000):
+def bLoss(outputs, labels, alpha = 1):
     #specifying the batch size
-    if torch.nonzero(torch.isnan(outputs)).sum().item() != 0: print(f'NaNs: {torch.nonzero(torch.isnan(outputs)).sum().item()}')
-    if torch.nonzero(torch.isinf(outputs)).sum().item() != 0: print(f'infs: {torch.nonzero(torch.isinf(outputs)).sum().item()}')
-
+    #if torch.nonzero(torch.isnan(outputs)).sum().item() != 0: print(f'NaNs: {torch.nonzero(torch.isnan(outputs)).sum().item()}')
+    #if torch.nonzero(torch.isinf(outputs)).sum().item() != 0: print(f'infs: {torch.nonzero(torch.isinf(outputs)).sum().item()}')
     outputs = torch.nan_to_num(outputs)
     batch_size = outputs.size()[0]
     se = ((outputs[:,0,0] - labels[:,0,0])**2 \
@@ -59,7 +59,7 @@ def bLoss(outputs, labels, alpha = 1000):
         + torch.maximum(torch.maximum(outputs[:,1,2]-1/2, -(outputs[:,1,2] + 1/2)), zero[:,0,0])**2 \
           )/6 \
         + (torch.maximum((3*torch.abs(eigs[:,1])-eigs[:,1])/2 - eigs[:,0],zero_eig)**2) \
-            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,0]),zero_eig)**2)
+            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,1]),zero_eig)**2)
     return (se+alpha*re).mean()
 
 def mseLoss(outputs, labels):
@@ -74,7 +74,50 @@ def mseLoss(outputs, labels):
           )/6
     return (se).mean()
 
-def realizLoss(outputs, labels, alpha = 1000):
+def realizLoss_components(outputs, labels, alpha = 1):
+    #specifying the batch size
+    outputs = torch.nan_to_num(outputs)
+    eigs = torch.sort(torch.real(torch.linalg.eigvals(outputs)),descending=True)[0]
+    zero = torch.zeros_like(outputs)
+    re = (torch.maximum(torch.maximum(outputs[:,0,0]-2/3, -(outputs[:,0,0] + 1/3)), zero[:,0,0])**2 \
+        + torch.maximum(torch.maximum(outputs[:,1,1]-2/3, -(outputs[:,1,1] + 1/3)), zero[:,0,0])**2 \
+        + torch.maximum(torch.maximum(outputs[:,2,2]-2/3, -(outputs[:,2,2] + 1/3)), zero[:,0,0])**2 \
+        + torch.maximum(torch.maximum(outputs[:,0,1]-1/2, -(outputs[:,0,1] + 1/2)), zero[:,0,0])**2 \
+        + torch.maximum(torch.maximum(outputs[:,0,2]-1/2, -(outputs[:,0,2] + 1/2)), zero[:,0,0])**2 \
+        + torch.maximum(torch.maximum(outputs[:,1,2]-1/2, -(outputs[:,1,2] + 1/2)), zero[:,0,0])**2 \
+          )/6 
+    return (alpha*re).mean()
+
+def realizLoss_eig(outputs, labels, alpha = 1):
+    #specifying the batch size
+    outputs = torch.nan_to_num(outputs)
+    eigs = torch.sort(torch.real(torch.linalg.eigvals(outputs)),descending=True)[0]
+    zero = torch.zeros_like(outputs)
+    zero_eig = torch.zeros_like(eigs[:,0])
+    re = (torch.maximum((3*torch.abs(eigs[:,1])-eigs[:,1])/2 - eigs[:,0],zero_eig)**2) \
+            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,1]),zero_eig)**2)
+    return (alpha*re).mean()
+
+def realizLoss_eig1(outputs, labels, alpha = 1):
+    #specifying the batch size
+    outputs = torch.nan_to_num(outputs)
+    eigs = torch.sort(torch.real(torch.linalg.eigvals(outputs)),descending=True)[0]
+    zero = torch.zeros_like(outputs)
+    zero_eig = torch.zeros_like(eigs[:,0])
+    re = (torch.maximum((3*torch.abs(eigs[:,1])-eigs[:,1])/2 - eigs[:,0],zero_eig)**2) 
+    return (alpha*re).mean()
+
+def realizLoss_eig2(outputs, labels, alpha = 1):
+    #specifying the batch size
+    outputs = torch.nan_to_num(outputs)
+    eigs = torch.sort(torch.real(torch.linalg.eigvals(outputs)),descending=True)[0]
+    zero = torch.zeros_like(outputs)
+    zero_eig = torch.zeros_like(eigs[:,0])
+    re = (torch.maximum(eigs[:,0] - (1/3 - eigs[:,1]),zero_eig)**2)
+    print(eigs)
+    return (alpha*re).mean()
+
+def realizLoss(outputs, labels, alpha = 1):
     #specifying the batch size
     outputs = torch.nan_to_num(outputs)
     batch_size = outputs.size()[0]
@@ -89,7 +132,7 @@ def realizLoss(outputs, labels, alpha = 1000):
         + torch.maximum(torch.maximum(outputs[:,1,2]-1/2, -(outputs[:,1,2] + 1/2)), zero[:,0,0])**2 \
           )/6 \
         + (torch.maximum((3*torch.abs(eigs[:,1])-eigs[:,1])/2 - eigs[:,0],zero_eig)**2) \
-            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,0]),zero_eig)**2)
+            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,1]),zero_eig)**2)
     return (alpha*re).mean()
 
 class TBNN(nn.Module):
@@ -111,6 +154,27 @@ class TBNN(nn.Module):
         gn = self.gn(x)
         b_pred = torch.sum(gn.view(-1,self.N,1,1)*torch.ones_like(Tn)*Tn,axis=1)
         return b_pred, gn
+    
+
+class MLP(nn.Module):
+    def __init__(self, input_dim: int, n_hidden: int, neurons: int, activation_function):
+        super().__init__()
+        self.input_dim = input_dim   
+
+        self.activation_function = activation_function
+        self.hidden = nn.ModuleList()
+        for k in range(n_hidden):
+            self.hidden.append(nn.Linear(input_dim, neurons))
+            input_dim = neurons  # For the next layer
+        self.output = nn.Linear(neurons, 5)
+                    
+    def forward(self, x):
+        for layer in self.hidden:
+            x = self.activation_function(layer(x))
+        output = self.output(x)
+        #b_pred = self.output(x).view(-1,3,3)
+        b_pred = torch.column_stack((output[:,0],output[:,1],output[:,2],output[:,1],output[:,3],output[:,4],output[:,2],output[:,4],-output[:,0]-output[:,3])).view(-1,3,3)
+        return b_pred
     
 class bDataset(Dataset):
     def __init__(self, df, input_features, scaler=None):
@@ -176,7 +240,7 @@ def count_nonrealizable(b):
         + torch.maximum(torch.maximum(b[:,1,2]-1/2, -(b[:,1,2] + 1/2)), zero[:,0,0])**2 \
           )/6 \
         + (torch.maximum((3*torch.abs(eigs[:,1])-eigs[:,1])/2 - eigs[:,0],zero_eig)**2) \
-            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,0]),zero_eig)**2)
+            + (torch.maximum(eigs[:,0] - (1/3 - eigs[:,1]),zero_eig)**2)
     n_nr = torch.count_nonzero(re)
     return n_nr
 
@@ -207,7 +271,7 @@ def early_stopped_tbnn_training_run(model_params, training_params, df_tv):
 
     optimizer = optim.Adam(model.parameters(), lr=training_params['learning_rate'])
     lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=training_params['learning_rate_decay'])
-    early_stopper = EarlyStopper(patience=training_params['early_stopping_patience'], min_delta=1E-7)
+    early_stopper = EarlyStopper(patience=training_params['early_stopping_patience'], min_delta=1E-6)
 
     print('EPOCH    LR        TRAIN     VALID         MSE:T/V              RL:T/V         %NR_t/%NR_v')
     print('=============================================================================================')
@@ -233,6 +297,8 @@ def early_stopped_tbnn_training_run(model_params, training_params, df_tv):
                 mse_v = mseLoss(y_pred_val,y).item()  
                 rl_v = realizLoss(y_pred_val,y).item() 
 
+        if val_loss_values[-1] < early_stopper.min_validation_loss:
+            best_model = copy.deepcopy(model)
         if early_stopper.early_stop(val_loss_values[-1]):
             print(f"{epoch:3d}   "
                   f"{lr_scheduler._last_lr[-1]:.3e}   "
@@ -253,4 +319,74 @@ def early_stopped_tbnn_training_run(model_params, training_params, df_tv):
                   f"{count_nonrealizable(y_pred_train)/len(y_pred_train)*100:.2f}% / {count_nonrealizable(y_pred_val)/len(y_pred_val)*100:.2f}%")
 
         lr_scheduler.step()
-    return model, loss_values, val_loss_values
+    return best_model, loss_values, val_loss_values
+
+def early_stopped_mlp_training_run(model_params, training_params, df_tv):
+    loss_fn = bLoss
+    loss_values = []
+    val_loss_values = []
+    df_valid = df_tv[df_tv['Case'].isin(training_params['val_set'])]
+    df_train = df_tv[~df_tv['Case'].isin(training_params['val_set'])]
+    tDs = bDataset(df_train, input_features=model_params['input_features'])
+    vDs = bDataset(df_valid, input_features=model_params['input_features'], scaler = tDs.X_scaler)
+    loader = DataLoader(tDs, shuffle=True, batch_size=training_params['batch_size'])
+
+    print(f'Training points: {len(df_train)}, validation points {len(df_valid)}')
+
+    model = MLP(input_dim = len(model_params['input_features']),
+                 n_hidden = model_params['n_hidden'],
+                 neurons = model_params['neurons'],
+                 activation_function = model_params['activation_function']
+                ).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=training_params['learning_rate'])
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=training_params['learning_rate_decay'])
+    early_stopper = EarlyStopper(patience=training_params['early_stopping_patience'], min_delta=1E-6)
+
+    print('EPOCH    LR        TRAIN     VALID         MSE:T/V              RL:T/V         %NR_t/%NR_v')
+    print('=============================================================================================')
+    for epoch in range(1, training_params['max_epochs']+1):
+        model.train()
+        for X_batch, T_batch, y_batch in loader:
+            y_pred = model(X_batch)
+            optimizer.zero_grad()
+            loss = loss_fn(y_pred, y_batch)
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        with torch.no_grad():
+            for X, T, y in DataLoader(tDs, shuffle=False, batch_size=tDs.__len__()):
+                y_pred_train = model(X)
+                loss_values.append(loss_fn(y_pred_train,y).item())
+                mse_t = mseLoss(y_pred_train,y).item()  
+                rl_t = realizLoss(y_pred_train,y).item()  
+            for X, T, y in DataLoader(vDs, shuffle=False, batch_size=vDs.__len__()):
+                y_pred_val = model(X)
+                val_loss_values.append(loss_fn(y_pred_val,y).item())   
+                mse_v = mseLoss(y_pred_val,y).item()  
+                rl_v = realizLoss(y_pred_val,y).item() 
+
+        if val_loss_values[-1] < early_stopper.min_validation_loss:
+            best_model = copy.deepcopy(model)
+        if early_stopper.early_stop(val_loss_values[-1]):
+            print(f"{epoch:3d}   "
+                  f"{lr_scheduler._last_lr[-1]:.3e}   "
+                  f"{loss_values[-1]:.5f}   "
+                  f"{val_loss_values[-1]:.5f}   "
+                  f"{mse_t:.5f} / {mse_v:.5f}   "
+                  f"{rl_t:.5f} / {rl_v:.5f}   "
+                  f"{count_nonrealizable(y_pred_train)/len(y_pred_train)*100:.2f}% / {count_nonrealizable(y_pred_val)/len(y_pred_val)*100:.2f}%")
+            break   
+
+        if (epoch % 10==0 or epoch==1):
+            print(f"{epoch:3d}   "
+                  f"{lr_scheduler._last_lr[-1]:.3e}   "
+                  f"{loss_values[-1]:.5f}   "
+                  f"{val_loss_values[-1]:.5f}   "
+                  f"{mse_t:.5f} / {mse_v:.5f}   "
+                  f"{rl_t:.5f} / {rl_v:.5f}   "
+                  f"{count_nonrealizable(y_pred_train)/len(y_pred_train)*100:.2f}% / {count_nonrealizable(y_pred_val)/len(y_pred_val)*100:.2f}%")
+
+        lr_scheduler.step()
+    return best_model, loss_values, val_loss_values
